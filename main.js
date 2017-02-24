658b2635d8f1af73b8fe42cdb27fecb107bb7b9f
Adrián Arroyo Calle <adrian.arroyocalle@gmail.com>
2017-02-24 11:35:45 +0100

    Update

--- main.js
+++ main.js
@@ -1,32 +1,38 @@
 /* Dos jugadores */
 
+/* LINKS */
+// https://www.codeandweb.com/physicseditor/tutorials/phaser-p2-physics-example-tutorial
+// https://phaser.io/examples/v2/p2-physics/load-polygon-2
+// https://phaser.io/examples/v2/p2-physics/contact-events
+// https://phaser.io/examples/v2/p2-physics/chain
+
 /* OBJETOS */
 
 var objetos = [{
     name: "Frigorífico",
     masa: 50,
     img: "frigo",
+    scale: [0.4,0.4]
+},{
+    name: "Carrito",
+    masa: 25,
+    img: "carrito",
     scale: [0.5,0.5]
 },{
-    name: "Enciclopedia",
-    masa: 25,
-    img: "enciclopedia.png",
-    scale: [1,1]
-},{
-    name: "Microondas",
+    name: "Televisión",
     masa: 15,
-    img: "microondas.png",
-    scale: [1,1]
+    img: "tele",
+    scale: [0.5,0.5]
 },{
-    name: "Impresora",
-    masa: "7",
-    img: "impresora.png",
-    scale: [1,1]
+    name: "Cohete",
+    masa: 87,
+    img: "cohete",
+    scale: [0.5,0.5]
 },{
-    name: "Silla",
-    masa: "14",
-    img: "silla.png",
-    scale: [1,1]
+    name: "Tonel",
+    masa: 14,
+    img: "tonel",
+    scale: [0.4,0.4]
 }]; // AÑADIR MÁS
 
 var state = "PLAY_PLAYER_SELECT"; // Otros estados son: PLAY_PLAYER_ANIM, PLAY_PLAYER_BOOST, WAIT_BROKE
@@ -37,18 +43,27 @@ var nubes = [];
 var intro, turnos = 0 ;
 var player = 1;
 var timeBoost = 0;
+var startTime = 0, topTime = 0;
 var personajes = [];
 var frame, alma, cropRect;
+var subiendo = true;
 
 function preload(){
     game.load.image("cielo","cielo.png");
     game.load.image("nube1","nube1-01.png");
     game.load.image("nube2","nube2-01.png");
     game.load.image("frigo","frigo.png");
+    game.load.image("tele","tele.png");
+    game.load.image("cohete","cohete.png");
+    game.load.image("carrito","carrito.png");
+    game.load.image("tonel","tonel.png");
 
     game.load.image("boost-frame","boost-frame.png");
     game.load.image("boost-alma","boost-alma.png");
 
+    game.load.physics("fisica","fisica.json");
+
+    game.load.spritesheet("barra","barra.png",800,75);
     game.load.spritesheet("torreIz", "torreIz.png",200,600);
     game.load.spritesheet("torreDe", "torreDe.png",200,600);
     game.load.spritesheet("personaje-verde","personaje-verde.png",600,600);
@@ -62,15 +77,22 @@ function create(){
     nubes[1] = game.add.sprite(50,120,"nube2");
     nubes[1].scale.setTo(0.2,0.2);
 
-    var cuerda = game.add.sprite();
+    var barra = game.add.sprite(145,270,"barra");
+    barra.scale.setTo(0.65,0.65);
+    barra.animations.add("barra1",[0,1,2,3,4],7,true);
+    barra.animations.add("barra2",[5,6,7,8,9],7,true);
+    barra.animations.add("barra3",[10,11,12,13,14],7,true);
+    barra.animations.add("barra4",[15,16,17,18,19],7,true);
+    barra.animations.add("barra5",[20,21,22,23,24],7,true);
+    barra.animations.play("barra1");
 
     var colIzq = game.add.sprite(150,300,"torreIz"); //new Phaser.Rectangle(200,400,50,200);
     colIzq.width = 100;
-    colIzq.animations.add("constant",[0,1,2,3,4],3,true);
+    colIzq.animations.add("constant",[0,1,2,3,4],7,true);
     colIzq.animations.play("constant");
     var colDer = game.add.sprite(550,300,"torreDe"); //new Phaser.Rectangle(550,400,50,200);
     colDer.width = 100;
-    colDer.animations.add("constant",[0,1,2,3,4],1,true);
+    colDer.animations.add("constant",[0,1,2,3,4],7,true);
     colDer.animations.play("constant");
 
     createSpriteObjetos();
@@ -81,6 +103,7 @@ function create(){
     personajes[0].frame = 0;
     personajes[0].animations.add("derecha",[24,25,26,27,28,28,29,30,31,32,33,34,35],20,true);
     personajes[0].animations.add("izquierda",[36,37,38,39,40,41,42,43,44,45,46,47],20,true);
+    personajes[0].animations.add("quieto", [48, 49, 50], 4, true); //Diferentes FPs P1 de P2 (razones estéticas)
 
     personajes[1] = game.add.sprite(-10,500,"personaje-rojo");
     personajes[1].width = 100;
@@ -88,8 +111,13 @@ function create(){
     personajes[1].frame = 0;
     personajes[1].animations.add("derecha",[24,25,26,27,28,28,29,30,31,32,33,34,35],20,true);
     personajes[1].animations.add("izquierda",[36,37,38,39,40,41,42,43,44,45,46,47],20,true);
+    personajes[1].animations.add("quieto", [48, 49, 50], 3, true);
+    
+    personajes[0].animations.play("quieto");
+    personajes[1].animations.play("quieto");
 
-    intro = game.add.text(20,550,"Jugador 1 - Turnos: 0",{
+
+    intro = game.add.text(20,30,"Jugador 1 - Turnos: 0",{
         font: "20pt Rainmaker",
         fill: "black"
     });
@@ -127,14 +155,52 @@ function update(){
         }
     }
     if(state == "PLAY_PLAYER_BOOST"){
-        cropRect.height = (alma.height*1500000000000000)/(timeBoost+1);
-        console.log(timeBoost);
+        var now = Date.now();
+        if(game.input.keyboard.isDown(Phaser.KeyCode.SPACEBAR)){
+            if(startTime == 0){
+                startTime = now;
+            }else{
+                timeBoost = now-startTime;
+                cropRect.height = 600 - timeBoost; //alma.height - (alma.height*1500)/timeBoost;
+                
+                alma.updateCrop();
+                if(cropRect.height < 0){
+                    startTime = now;
+                    cropRect.height = alma.height;
+                    alma.updateCrop();
+                }
+            }
+        }else if(timeBoost > 0){
+            var penalizacion = 1;
+            var change = Math.abs(600 - timeBoost);
+            console.log("Change: "+change)
+            if (change > 50){
+                penalizacion = Math.ceil(Math.pow(change / 100,2));
+                console.log("Penalizacion: "+penalizacion);
+            }
+            var penalizacionText = game.add.text(300,350,"Fuerza x"+penalizacion,{
+                font: "20pt Rainmaker",
+                fill: "black"
+            });
+            setTimeout(function(){
+                penalizacionText.destroy();   
+            },1000);
+            startTime = 0;
+            timeBoost = 0;
+            alma.destroy();
+            frame.destroy();
+            nextState();
+        }
+
+        /*var now = Date.now();
+        cropRect.height = (alma.height*1500)/(now - timeBoost+1);
+        console.log(now - timeBoost);
         console.log(cropRect);
         alma.updateCrop();
-        if( (timeBoost == 0 || timeBoost > 1500 ) && game.input.keyboard.isDown(Phaser.KeyCode.SPACEBAR)){
-            timeBoost = Date.now();
+        if( (timeBoost == 0 || (now - timeBoost) > 1500 ) && game.input.keyboard.isDown(Phaser.KeyCode.SPACEBAR)){
+            timeBoost = now;
         }else if(timeBoost > 0){
-            var boost = Date.now() - timeBoost;
+            var boost = now - timeBoost;
             var change = Math.abs(boost - 666) // 666ms es el tiempo perfecto (poner un tiempo especifico a cada objeto?)
             var penalizacion = 0;
             if(change > 200){
@@ -142,7 +208,7 @@ function update(){
             }
             timeBoost = 0;
             nextState();
-        }
+        }*/
     }
     if(state == "WAIT_BROKE"){
         personajes[player - 1].x -= 3;
@@ -179,11 +245,11 @@ function nextState(){
             state = "PLAY_PLAYER_BOOST";
             personajes[player - 1].animations.stop();
             personajes[player -1].frame = 0;
-            frame = game.add.sprite(700,100,"boost-frame");
-            frame.scale.setTo(0.75,0.75);
             alma = game.add.sprite(700,100,"boost-alma");
             alma.scale.setTo(0.75,0.75);
-            cropRect = new Phaser.Rectangle(0,0,alma.width,alma.height*1500/(timeBoost+1));
+            frame = game.add.sprite(700,100,"boost-frame");
+            frame.scale.setTo(0.75,0.75);
+            cropRect = new Phaser.Rectangle(0,0,100,600);
             alma.crop(cropRect);
         }
             break;
@@ -193,7 +259,7 @@ function nextState(){
         }break;
         case "WAIT_BROKE":{
             resetSelectedObject();
-            personajes[player - 1].animations.stop();
+            personajes[player - 1].animations.play("quieto");
             personajes[player - 1].frame = 0;
             changePlayer();
             state = "PLAY_PLAYER_SELECT"; 
